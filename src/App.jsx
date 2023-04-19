f50ba1598e586abda5e196b98f93c1112c522e0d
Ajnas N B <114915484+AjnasNB@users.noreply.github.com>
2023-04-19 12:28:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading";
 import Weather from "./components/Weather";
 
-function App() {
-  
-  return (
-    <div className="App">
-    <Weather/>
-    </div>
-  );
-}
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return isLoading ? <Loading /> : <Weather />;
+};
 
 export default App;
